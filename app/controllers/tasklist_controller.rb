13f9b4a6e143721016abbdd733b80592588c672a
Gaku <change.in2@icloud.com>
2021-05-09 02:12:22 +0000

    task model2

---
+++ app/controllers/tasklist_controller.rb
@@ -0,0 +1,23 @@
+class TasklistController < ApplicationController
+  def index
+      @tasklist =tasklist.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
